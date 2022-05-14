d7dfc81ce334cf6213ecf8e18c0e5d38dad27983
AdrianHerrera_ <neomach55@gmail.com>
2022-05-14 12:09:48 -0500

    Google tag manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { GoogleTagManagerService } from 'angular-google-tag-manager';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'siuroficialsite';
+
+  constructor(
+    private gtmService: GoogleTagManagerService,
+    private router: Router
+  ) {
+    this.gtmService.addGtmToDom();
+  }
+
+  ngOnInit(): void {
+    this.router.events.forEach((item) => {
+      if (item instanceof NavigationEnd) {
+        const gtmTag = {
+          event: 'page',
+          pageName: item.url,
+        };
+
+        this.gtmService.pushTag(gtmTag);
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Analythics
+import { GoogleTagManagerModule } from 'angular-google-tag-manager';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -11,6 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    GoogleTagManagerModule.forRoot({
+      id: 'GTM-MQ47FD5',
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
